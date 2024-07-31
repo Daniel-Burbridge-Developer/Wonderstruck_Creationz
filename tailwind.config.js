09cb465225b8b6c0bc1383c232a919a839757b35
Daniel <daniel.burbridge.developer@gmail.com>
2024-07-31 20:00:49 +0800

    making images work

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export const content = ['./index.html', './src/**/*.{js,ts,jsx,tsx}'];
 export const theme = {
   extend: {
     backgroundImage: {
-      'backdrop-paintsplash': "url('./assets/backdrops/paintsplash.png')",
+      'backdrop-paintsplash': "url('/assets/backdrops/paintsplash.png')",
     },
   },
 };
